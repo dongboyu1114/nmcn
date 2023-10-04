2b341bb96dfd97c3a7f11fb1ed574d58a6c87ec5
dby <dongboyu1114@163.com>
2023-10-04 23:52:31 +0800

    三端合一基本实现

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,41 @@
 <template>
-	<view class="layout">
+	<view class="login">
+		<view class="content-wrapper">
+			<view class="title">
+				<h1>纳米采暖</h1>
+			</view>
+			<view class="login-form">
+				<view class="login-form-items">
+					<view class="login-form-items-title">手机号</view>
+					<input type="text" class="login-input" placeholder="手机号" />
+				</view>
+
+				<view class="login-form-items">
+					<view class="login-form-items-title">登录密码</view>
+					<input :type="pwd_show?'text':'password'" class="login-input" placeholder="请输入登录密码" />
+					<image class="password_img" @click="change_pwd" style="width: 50px; height: 30px;"
+						:src="pwd_show? '/static/show_pswd.png':'/static/no_pswd.png'"></image>
+				</view>
+
+				<view class="login-form-items">
+					<view class="login-form-items-title">验证码</view>
+					<input type="text" class="login-input" placeholder="请输入验证码" />
+					<view class="captcha-wrapper">
+						<image class="img_code" src="/static/code.png"></image>
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="submit-wrapper">
+			<button style="background-color: aqua;" type="primary" class="login-btn" @click="handleClick">登录</button>
+		</view>
 	</view>
 </template>
 
 <script setup>
+	import {
+		ref
+	} from 'vue'
 	import {
 		sendAuthCodeToBackend,
 		getAllDataFromNami
@@ -33,6 +65,16 @@
 		// alert(code)
 		loginCheckNow()
 	})
+	//点击事件
+	let pwd_show = ref(false)
+	const change_pwd = () => {
+		pwd_show.value = !pwd_show.value;
+	};
+	const handleClick = () => {
+		uni.redirectTo({
+			url: '/pages/register/index'
+		});
+	};
 	// 微信登录处理
 	const loginCheckNow = async () => {
 		if (isWeixinH5()) {
@@ -58,7 +100,6 @@
 			}
 		} else if (isApplet()) {
 			if (tokenAvailable()) {
-
 				uni.redirectTo({
 					url: '/pages/gift/index'
 				});
@@ -71,7 +112,7 @@
 				}
 			}
 		} else if (isApp()) {
-			console.log('登陆检查aaa');
+			handleClick()
 		} else {
 			console.error('不在微信环境下');
 			// 在不在微信环境下的处理逻辑
@@ -111,3 +152,71 @@
 		}
 	}
 </script>
+
+<style lang="scss">
+	page {
+		background: #F4F5F6;
+	}
+
+	.img_code {
+		width: 60px;
+		height: 30px;
+	}
+
+	.login {
+		.content-wrapper {
+			width: 100%;
+
+			.title {
+				margin-top: 35rpx;
+				width: 100%;
+				margin-bottom: 10px;
+
+				h1 {
+					border: 0px;
+					width: 60%;
+					margin: 0 auto;
+					text-align: center;
+					border-bottom: 1px solid #E3E3E3;
+					height: 50px;
+					line-height: 50px;
+					font-size: 17px;
+					overflow: hidden;
+					font-weight: 400;
+				}
+			}
+
+			.login-form {
+				margin: 20px 10px 20px 15px;
+				background: #FFFFFF;
+
+				.login-form-items {
+					padding: 15px 10px;
+					border-bottom: 1px solid #F3F4F5;
+					position: relative;
+					display: -webkit-flex;
+					display: flex;
+
+					.login-form-items-title {
+						width: 30%;
+						line-height: 22px;
+						height: 22px;
+						flex-shrink: 0;
+					}
+
+					.login-input {
+						width: 100%
+					}
+
+				}
+			}
+		}
+
+		.submit-wrapper {
+			padding: 10px;
+			padding-top: 10px;
+			background-color: 11ffff;
+		}
+
+	}
+</style>
